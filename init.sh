a1e5bd09392c156f2bcd103ef055f208ab3588d4
la1n23 <jack.frost2323@proton.me>
2025-02-06 04:54:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sed -i 's/robbyrussell/norm/g' ~/.zshrc
 sudo apt update && sudo apt install -y fzf
 sed -i 's/(git)/(git fzf)/g' ~/.zshrc
 /usr/bin/setxkbmap -option "ctrl:nocaps"
-
+/bin/zsh
 #echo 'export VISUAL=nvim' >> .zshrc
 #echo 'export EDITOR="$VISUAL"' >> .zshrc
 #bash -c "$(curl -fsSL https://raw.githubusercontent.com/jdhao/nvim-config/refs/heads/main/docs/nvim_setup_linux.sh)"
