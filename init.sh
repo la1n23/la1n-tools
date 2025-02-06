34c7a20e4e0937cf970e8d3ec5aaed1872470f64
la1n23 <jack.frost2323@proton.me>
2025-02-06 07:51:40 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i 's/robbyrussell/norm/g' ~/.zshrc
+sed -i 's/robbyrussell/funky/g' ~/.zshrc
 sudo apt update && sudo apt install -y fzf atool
 sed -i 's/(git)/(git fzf nmap)/g' ~/.zshrc
+# doesn work on parrotOS ((
 /usr/bin/setxkbmap -option "ctrl:nocaps"
 sudo chsh $(whoami) -s /bin/bash
 /bin/zsh -i
+# TODO: find or create nice nvim config
 #echo 'export VISUAL=nvim' >> .zshrc
 #echo 'export EDITOR="$VISUAL"' >> .zshrc
 #bash -c "$(curl -fsSL https://raw.githubusercontent.com/jdhao/nvim-config/refs/heads/main/docs/nvim_setup_linux.sh)"
