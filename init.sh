413e4199c3326d89bb7466df961739389b4e86c6
Lain Iwakura <jack.frost2323@proton.me>
2025-02-07 14:45:26 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,22 @@ sed -i 's/(git)/(git fzf nmap)/g' ~/.zshrc
 /usr/bin/setxkbmap -option "ctrl:nocaps"
 /usr/bin/setxkbmap -option "caps:ctrl_modifier"
 sudo chsh $(whoami) -s $(which zsh)
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cat << DESU >> ~/.vimrc
+call plug#begin('~/.vim/plugged')"
+Plug 'vim-perl/vim-perl', { 'for': 'perl', 'do': 'make clean carp dancer highlight-all-pragmas moose test-more try-tiny' }"
+call plug#end()
+set autoindent expandtab tabstop=2 shiftwidth=2
+set autoindent
+set number
+DESU
+
+vim -c PlugInstall +qa
+
 /bin/zsh -i
+
 # TODO: find or create nice nvim config
 #echo 'export VISUAL=nvim' >> .zshrc
 #echo 'export EDITOR="$VISUAL"' >> .zshrc
