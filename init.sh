c8b78d7217bddafa81cb0b421d06e4c1fa097ccf
Lain Iwakura <jack.frost2323@proton.me>
2025-02-08 02:46:05 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ sudo chsh $(whoami) -s $(which zsh)
 ### VIM
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cat << DESU >> ~/.vimrc
+cat << DESU > ~/.vimrc
 call plug#begin('~/.vim/plugged')"
 Plug 'vim-perl/vim-perl', { 'for': 'perl', 'do': 'make clean carp dancer highlight-all-pragmas moose test-more try-tiny' }"
 call plug#end()
