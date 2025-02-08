34517923954c62b3d345870a20e394d3e8ebb970
la1n23 <jack.frost2323@proton.me>
2025-02-08 06:01:52 +0300

    vpn draft

---
+++ vpn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "cat /home/$(whoami)/openconnect | openconnect --user $VPN_USER --servercert pin-sha256:$VPN_CERT_HASH --passwd-on-stdin $VPN_DOMAIN:$VPN_PORT"
+sudo bash -c "cat /home/$(whoami)/openconnect | openconnect --user $VPN_USER --servercert pin-sha256:$VPN_CERT_HASH --passwd-on-stdin $VPN_DOMAIN:$VPN_PORT"
