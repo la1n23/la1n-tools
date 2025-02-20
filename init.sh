4e75b4eceb9dad522a0b23f99e23182df6a494c4
Lain Iwakura <jack.frost2323@proton.me>
2025-02-20 11:00:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,6 +16,8 @@ echo 'export TERM=xterm-256color' >> ~/.zshrc
 curl 'https://raw.githubusercontent.com/la1n23/la1n-tools/refs/heads/master/x-lpha3ch0.zsh-theme' > ~/.oh-my-zsh/themes/x-lpha3ch0.zsh-theme
 echo "export PATH=/home/$(whoami)/la1n-tools:$PATH" >> ~/.zshrc
 
+# gdb enhancer
+bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
 # ParrotOS is shit and gay - csh doesnt work
 echo "zsh -i" >> ~/.bashrc
